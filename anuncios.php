f6f00974fb5ed0a2a8e2ffa99b3d5cd1bc093fe9
sxshita <racagnisasha@gmail.com>
2022-01-07 20:46:22 -0300

    Se agrega el proyecto

---
+++ anuncios.php
@@ -0,0 +1,198 @@
+<?php 
+    require 'includes/funciones.php';
+    incluirTemplate('header'); 
+?>
+
+    <main class="contenedor seccion">
+        <div class="contenedor-anuncios">
+            <div class="anuncio">
+                <picture>
+                    <source srcset="build/img/anuncio1.webp" type="image/webp">
+                    <source srcset="build/img/anuncio1.jpg" type="image/jpeg">
+                    <img loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa de Lujo en el lago</h3>
+                    <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
+                    <p class="precio">$3,000,000</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                            <p>4</p>
+                        </li>
+                    </ul>
+                    <a href="anuncio.html" class="boton-amarillo-block">
+                        Ver Propiedad
+                    </a>
+                </div><!--.contenido-anuncio-->
+            </div><!--.anuncio-->
+
+            <div class="anuncio">
+                <picture>
+                    <source srcset="build/img/anuncio2.webp" type="image/webp">
+                    <source srcset="build/img/anuncio2.jpg" type="image/jpeg">
+                    <img loading="lazy" src="build/img/anuncio2.jpg" alt="anuncio">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa Terminados de Lujo</h3>
+                    <p>Casa en el lago con diseño modernoo, así como tecnología inteligente</p>
+                    <p class="precio">$2,000,000</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                            <p>4</p>
+                        </li>
+                    </ul>
+                    <a href="anuncio.html" class="boton-amarillo-block">
+                        Ver Propiedad
+                    </a>
+                </div><!--.contenido-anuncio-->
+            </div><!--.anuncio-->
+
+            <div class="anuncio">
+                <picture>
+                    <source srcset="build/img/anuncio3.webp" type="image/webp">
+                    <source srcset="build/img/anuncio3.jpg" type="image/jpeg">
+                    <img loading="lazy" src="build/img/anuncio3.jpg" alt="anuncio">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa con Alberca</h3>
+                    <p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
+                    <p class="precio">$3,000,000</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                            <p>4</p>
+                        </li>
+                    </ul>
+                    <a href="anuncio.html" class="boton-amarillo-block">
+                        Ver Propiedad
+                    </a>
+                </div><!--.contenido-anuncio-->
+            </div><!--.anuncio-->
+
+            <div class="anuncio">
+                <picture>
+                    <source srcset="build/img/anuncio4.webp" type="image/webp">
+                    <source srcset="build/img/anuncio4.jpg" type="image/jpeg">
+                    <img loading="lazy" src="build/img/anuncio4.jpg" alt="anuncio">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa con Alberca</h3>
+                    <p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
+                    <p class="precio">$3,000,000</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                            <p>4</p>
+                        </li>
+                    </ul>
+                    <a href="anuncio.html" class="boton-amarillo-block">
+                        Ver Propiedad
+                    </a>
+                </div><!--.contenido-anuncio-->
+            </div><!--.anuncio-->
+
+
+            <div class="anuncio">
+                <picture>
+                    <source srcset="build/img/anuncio5.webp" type="image/webp">
+                    <source srcset="build/img/anuncio5.jpg" type="image/jpeg">
+                    <img loading="lazy" src="build/img/anuncio5.jpg" alt="anuncio">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa con Alberca</h3>
+                    <p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
+                    <p class="precio">$3,000,000</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img class="iconos" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="iconos" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="iconos" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                            <p>4</p>
+                        </li>
+                    </ul>
+                    <a href="anuncio.html" class="boton-amarillo-block">
+                        Ver Propiedad
+                    </a>
+                </div><!--.contenido-anuncio-->
+            </div><!--.anuncio-->
+
+            <div class="anuncio">
+                <picture>
+                    <source srcset="build/img/anuncio6.webp" type="image/webp">
+                    <source srcset="build/img/anuncio6.jpg" type="image/jpeg">
+                    <img loading="lazy" src="build/img/anuncio6.jpg" alt="anuncio">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa con Alberca</h3>
+                    <p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
+                    <p class="precio">$3,000,000</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img class="iconos" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="iconos" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img class="iconos" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                            <p>4</p>
+                        </li>
+                    </ul>
+                    <a href="anuncio.html" class="boton-amarillo-block">
+                        Ver Propiedad
+                    </a>
+                </div><!--.contenido-anuncio-->
+            </div><!--.anuncio-->
+
+        </div><!--.contenedor-anuncios-->
+    </main>
+
+    <?php incluirTemplate('footer'); ?>
